cdbf222b9d9b770db6fe2ff8be3ebfa1150ea811
Howard Miller <howardsmiller@gmail.com>
2021-11-17 11:48:48 +0000

    working on front page blocks

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="content">
-
+      <div v-for="pageitem in pageitems" :key="pageitem._id" class="mt-1 rounded bg-light">
+          <div class="row">
+              <div class="col-7">
+                  <h2>{{ pageitem.Heading }}</h2>
+              </div>
+              <div class="col-5">
+                  <img :src="wwwroot + pageitem.Image.path" />
+              </div>    
+          </div>
+      </div>
   </div>
 </template>
 
@@ -13,18 +22,20 @@ export default {
   },
   data:function() {
       return {
-          pageitems: null
+          pageitems: null,
+          wwwroot: '',
       }
   },
   mounted() {
         const token = process.env.VUE_APP_TOKEN
-        const cms = process.env.VUE_APP_CMS
+        const api = process.env.VUE_APP_API
+        this.wwwroot = process.env.VUE_APP_ROOT
 
         const v = this
-        axios.get(cms + 'collections/get/FrontPageItems?token=' + token)
+        axios.get(api + 'collections/get/FrontPageItems?token=' + token)
         .then(response => {
             window.console.log(response)
-            v.pageitems = response.data
+            v.pageitems = response.data.entries
         })
   }
 }
